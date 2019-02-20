38a1d5f2a9e7dc7a6b4974e7ce0eb97d6ae69240
mcorielly <mcorielly@gmail.com>
2019-02-20 15:32:14 -0400

    Commit developed of Sale Model and DetailSeal Model

--- app/DetailSale.php
+++ app/DetailSale.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetailSale extends Model
 {
-    //
+    protected $fillable = [
+    	'client_id', 'user_id', 'type_voucher', 'serie_voucher', 'num_vaucher', 'date_time', 'tax', 'total', 'status' 
+    ];
 }
--- app/Sale.php
+++ app/Sale.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    protected $fillable = [
+    	'sale_id', 'product_id', 'type_voucher', 'quantity', 'price', 'discount' 
+    ];
+
+    public $timestamps = false;
 }
