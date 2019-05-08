6524fee6c66b13ba2a7f42cddad3c61215cdcc25
Miguel Angel Cornielly H <mcornielly@gmail.com>
2019-05-08 09:38:29 -0400

    Config User

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,21 +16,21 @@ class UsersTableSeeder extends Seeder
         	'id' => 1,
         	'role_id' => 1,
         	'user_name' => 'admin',
-        	'password' => bcrypt('1234'),
+        	'password' => bcrypt('123456'),
         ]);
 
         User::create([
         	'id' => 2,
         	'role_id' => 2,
         	'user_name' => 'vendedor',
-        	'password' => bcrypt('1234'),
+        	'password' => bcrypt('123456'),
         ]);
 
         User::create([
         	'id' => 3,
         	'role_id' => 3,
         	'user_name' => 'deposito',
-        	'password' => bcrypt('1234'),
+        	'password' => bcrypt('123456'),
         ]);
     }
 }
